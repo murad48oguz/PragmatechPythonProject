d922202f55125a2983b023a68f2f4b0feb75cd71
murad48oguz <ebulzade@inbox.ru>
2021-06-30 00:52:39 +0400

    FirstModel

--- projectgeneral/home/models.py
+++ projectgeneral/home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Attorney (models.Model):
+class Attorney(models.Model):
     name = models.CharField(max_length=40)
     surname = models.CharField(max_length=40)
     absolutejob = models.CharField(max_length=40)
