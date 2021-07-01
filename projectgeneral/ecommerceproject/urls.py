429ae3a68453ef734fd0ef1c7f9611c69cc4dace
murad48oguz <ebulzade@inbox.ru>
2021-07-01 12:39:01 +0400

    MediaFilesSettings

--- projectgeneral/ecommerceproject/urls.py
+++ projectgeneral/ecommerceproject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from home import views
 
 urlpatterns = [
@@ -32,4 +34,4 @@ urlpatterns = [
     path('productdetail/',views.productdetail),
     # path('myaccount/',views.myaccount),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
