45f6d10e8ea351248a78511a80fb56e09e6d513d
murad48oguz <ebulzade@inbox.ru>
2021-07-07 12:49:35 +0400

    AdditionToModels

--- projectgeneral/home/models.py
+++ projectgeneral/home/models.py
@@ -13,14 +13,22 @@ class Attorney(models.Model):
     def __str__(self):
         return self.name
     
-class Contact():
+class Contact(models.Model):
     name = models.CharField(max_length=30)
     email= models.EmailField()
     case_details = models.TextField()
 
 
-class FreeCase():
+class FreeCase(models.Model):
    name = models.CharField(max_length=30)
    email= models.EmailField()
    subject = models.CharField(max_length=30)
    case_details = models.TextField()
+
+class Shop(models.Model):
+    stuffname = models.CharField(max_length = 40)
+    image = models.ImageField() 
+    price = models.IntegerField()
+    status = models.BooleanField(blank=True)
+    
+    
