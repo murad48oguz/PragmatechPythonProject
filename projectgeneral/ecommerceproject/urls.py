da36d2e382260a85292c84990e9defdc601860e5
murad48oguz <ebulzade@inbox.ru>
2021-07-19 17:57:01 +0400

    TemplateIssues

--- projectgeneral/ecommerceproject/urls.py
+++ projectgeneral/ecommerceproject/urls.py
@@ -19,6 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from home import views
 
+app_name = 'home'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('jet/', include('jet.urls', 'jet')),  
